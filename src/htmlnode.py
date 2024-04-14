057e01e0027d2018a34704d2052f8e81b5bc0ba8
Julian Karl <95536830+Veymore@users.noreply.github.com>
2024-04-14 22:03:42 +0200

    [Chapter2,5] ParentNode + UnitTest reworking + param structure aligned
    - created ParentNode
    - created ParentNode UnitTests
    - reworked parameters for all HTMLNode classes and children to have the same order
    - check for required parameters in constructor and raise exceptions accordingly
    - UnitTests for Exceptions

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,27 +20,29 @@ class HTMLNode:
 
     def props_to_html(self) -> str:
         prop_to_html = ""
+        if self.props == None:
+            return ""
         for prop in self.props.items():
             prop_to_html += f' {prop[0]}="{prop[1]}"'
         return prop_to_html
 
     def __repr__(self) -> str:
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"{self.__class__.__name__}({self.tag}, {self.value}, {self.children}, {self.props})"
 
 
 # MARK: LeafNode
 class LeafNode(HTMLNode):
     def __init__(
         self,
-        value: str,
         tag: str = None,
+        value: str = None,
         props: dict[str, str] = None,
     ) -> None:
+        if value == None:
+            raise ValueError("Value is none. All LeafNodes require a value.")
         super().__init__(tag, value, None, props)
 
     def to_html(self) -> str:
-        if self.value == None:
-            raise ValueError("Value is none. All LeafNodes require a value.")
         if self.tag == None:
             return self.value
         if self.props != None:
@@ -48,3 +50,26 @@ class LeafNode(HTMLNode):
         else:
             rendered_props = ""
         return f"<{self.tag}{rendered_props}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str = None,
+        children: list[HTMLNode] = None,
+        props: dict[str, str] = None,
+    ) -> None:
+        if tag == None:
+            raise ValueError("ParentNode needs to have a tag assigned.")
+        if children == None:
+            raise ValueError("ParentNode needs to have children.")
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        rendered_props = self.props_to_html()
+        rendered_children = ""
+        if self.children is not None:
+            for child in self.children:
+                rendered_child = child.to_html()
+                rendered_children += rendered_child
+        return f"<{self.tag}{rendered_props}>{rendered_children}</{self.tag}>"
