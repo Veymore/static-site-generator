79a5f74e7abf2a782e783f302b10a1db29f49f39
Veymore <95536830+Veymore@users.noreply.github.com>
2024-04-14 22:11:49 +0200

    [Misc] Cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,2 @@
 from textnode import TextNode as tn
 from htmlnode import HTMLNode as hn
-
-testnode = hn(None, None, None, {"href": "https://www.google.com", "target": "_blank"})
-
-print(testnode.props_to_html())
-
-print(testnode)
